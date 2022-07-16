e944e936c6f6aa2479200451b2e623919126a981
Tomas Štefe <tomas.stefe@gmail.com>
2022-07-16 16:36:30 +0200

    Building city search component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'aeec8d6909mshe7a0b9347cfce56p10011ajsn7d783592f057',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
