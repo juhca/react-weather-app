7b55a1b27b0a93bab3009acf441a2f5156fd880e
Tomas Štefe <tomas.stefe@gmail.com>
2022-07-17 18:15:26 +0200

    Building current weather component + icons

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'YOUR API KEY',
+		'X-RapidAPI-Key': 'aeec8d6909mshe7a0b9347cfce56p10011ajsn7d783592f057',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
